2df5c8930aa53cb9c702694b582a812cf42b1d6b
Josh Ourisman <me@josho.io>
2021-02-20 11:32:53 -0500

    Somewhat successfully testing different settings from environment.

--- tests/test_env.py
+++ tests/test_env.py
@@ -1,8 +1,21 @@
+import os
+from unittest.mock import patch
+
 from django.conf import settings
+from django.utils.functional import empty
 from pydantic_settings import SetUp
 
 
+@patch.dict(os.environ, clear=True, DATABASE_URL="sqlite:///foo")
 def test_env_loaded():
     SetUp().configure()
 
-    assert settings.DATABASES["default"]["NAME"] == "db"
+    assert settings.DATABASES["default"]["NAME"] == "foo"
+
+
+@patch.dict(os.environ, clear=True, DATABASE_URL="sqlite:///bar")
+def test_env_loaded2():
+    settings._wrapped = empty
+    SetUp().configure()
+
+    assert settings.DATABASES["default"]["NAME"] == "bar"
