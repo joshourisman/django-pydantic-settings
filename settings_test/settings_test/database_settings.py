2650c8020792ea14ad80cd0df11dff1f90404e10
Josh Ourisman <me@josho.io>
2022-04-22 21:00:42 -0400

    Properly setting DATABASES in tests.

--- settings_test/settings_test/database_settings.py
+++ settings_test/settings_test/database_settings.py
@@ -1,12 +1,13 @@
 from pydantic import BaseSettings, Field
 from pydantic_settings import PydanticSettings
+from pydantic_settings.settings import DatabaseSettings
 from pydantic_settings.database import DatabaseDsn
 
 
-class DatabaseSettings(BaseSettings):
+class Databases(DatabaseSettings):
     DEFAULT: DatabaseDsn = Field(env="DATABASE_URL")
     SECONDARY: DatabaseDsn = Field(env="SECONDARY_DATABASE_URL")
 
 
 class TestSettings(PydanticSettings):
-    DATABASES: DatabaseSettings = Field({})
+    DATABASES: Databases = Field({})
